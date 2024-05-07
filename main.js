fc9b26bb6d43be6cf4ac5ff65056022192fe22ae
Ghulam Akber <160354667+Akber261986@users.noreply.github.com>
2024-05-08 01:10:38 +0500

    count down v 1.0.0

---
+++ main.js
@@ -0,0 +1,37 @@
+#! /usr/bin/env node
+import { differenceInSeconds } from "date-fns";
+import inquirer from "inquirer";
+let res = await inquirer.prompt({
+    name: "userinput",
+    type: "number",
+    message: "Please Enter Time",
+    validate: (input) => {
+        if (isNaN(input)) {
+            return "Please Enter a Valid Number";
+        }
+        else if (input > 60) {
+            return "Please Enter a Number b/w (1 - 60)";
+        }
+        else {
+            return true;
+        }
+    }
+});
+let input = res.userinput;
+function setTime(val) {
+    const intTime = new Date().setSeconds(new Date().getSeconds() + val + 2);
+    const intervalTime = new Date(intTime);
+    setInterval(() => {
+        const currentTime = new Date();
+        const timeDiff = differenceInSeconds(intervalTime, currentTime);
+        if (timeDiff <= 0) {
+            console.log(`00:00
+Timer Has stoped`);
+            process.exit();
+        }
+        const min = Math.floor((timeDiff % (3600 * 24)) / 3600);
+        const sec = Math.floor(timeDiff % 60);
+        console.log(`${min.toString().padStart(2, "0")}:${sec.toString().padStart(2, "0")}`);
+    }, 1000);
+}
+setTime(input);
